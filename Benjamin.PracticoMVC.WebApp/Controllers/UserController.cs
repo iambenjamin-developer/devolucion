cd3a32258c2bf7dda7846ec691b3fb3281c86dc6
Your Name <you@example.com>
2020-05-18 15:15:06 -0300

    devolucion

---
+++ Benjamin.PracticoMVC.WebApp/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Benjamin.PracticoMVC.WebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult List(Models.Test.TestModel modelParametro)
+        {
+
+            var model = new Models.Test.TestModel();
+
+            if (modelParametro.Mensaje != null)
+            {
+                model.Mensaje = modelParametro.Mensaje;
+            }
+
+            AccesoDatos.Test metodos = new AccesoDatos.Test();
+
+            List<Entidades.Join_UsuariosRoles> listaUyR = metodos.ListarUsuariosRoles();
+
+
+
+            model.ListaDeUsuariosyRoles = listaUyR;
+
+            return View(model);
+
+
+        }
+    }
+}
